1f23c28cedce14c8cff0a5b6eac5f687ddea53c0
JetBoi <dmitry.kapustin@jetstyle.ru>
2018-09-18 19:49:24 +0500

    + сервер отдает весь билд по запросу

--- code/API.js
+++ code/API.js
@@ -1,5 +1,17 @@
 module.exports =
 {
+    GET_game_get: "/",
+    GET_game_build: function(matchUrl)
+    {
+        if(matchUrl.indexOf("/Build") === 0)
+            return true;
+    },
+    GET_game_templateData: function(matchUrl)
+    {
+        if(matchUrl.indexOf("/TemplateData") === 0)
+            return true;
+    },
+
     GET_Leaderboard_get: "/api/leaderboard/get",
     POST_Leaderboard_update: "/api/leaderboard/update"
 };
--- code/main.js
+++ code/main.js
@@ -20,17 +20,30 @@ var server = http.createServer((req, res) =>
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    // process user request
+    // set no cache
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+    res.setHeader("Expires", "0"); // Proxies.
 
-    // на гет - выплевываем всю лидерборду
+    // process user requests
     if(req.method.toLocaleLowerCase() === "get")
     {
-        if(req.url === API.GET_Leaderboard_get)
+        // отдаем главную страницу
+        if (API.GET_game_get)
+            res.end(fs.readFileSync("../../build/index.html"));
+           
+        // отдаем билд и контент
+        if (API.GET_game_build(req.url) || API.GET_game_templateData(req.url))
+            res.end(fs.readFileSync("../../build" + req.url));
+
+        // отдаем лидерборду
+        if (API.GET_Leaderboard_get)
             res.end(leadboard.GetJSONString());
+
     }
-    // на пост - парсим новую запись и передаем ее на обработку в лидерборду
     else if(req.method.toLowerCase() === "post")
     {
+        // парсим новую запись и передаем ее на обработку в лидерборду
         if(req.url === API.POST_Leaderboard_update)
         {
             req.on("data", (chunk) => 
